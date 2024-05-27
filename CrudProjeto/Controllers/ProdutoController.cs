245399acea3818a423fd35da8b6d627677ff0d6a
Lucas Piccoli <lucaspiccoli70@gmail.com>
2024-05-27 13:44:18 -0300

    Adicionar arquivos de projeto.

---
+++ CrudProjeto/Controllers/ProdutoController.cs
@@ -0,0 +1,41 @@
+﻿using CrudProjetoClassLibrary.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudProjeto.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private static List<Produto> Produtos = new List<Produto>
+        {
+            new Produto { Id = 1, Nome = "Produto A", Preco = 10.0m },
+            new Produto { Id = 2, Nome = "Produto B", Preco = 20.0m },
+            new Produto { Id = 3, Nome = "Produto C", Preco = 30.0m }
+        };
+        public IActionResult Index()
+        {
+            return View(Produtos);
+        }
+        [HttpPost]
+        public IActionResult Edit(Produto produto)
+        {
+            var prod = Produtos.FirstOrDefault(p => p.Id == produto.Id);
+            if (prod != null)
+            {
+                prod.Nome = produto.Nome;
+                prod.Preco = produto.Preco;
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var prod = Produtos.FirstOrDefault(p => p.Id == id);
+            if (prod != null)
+            {
+                Produtos.Remove(prod);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
